c1126257e6eb139632fa9fff6f1387449a666f05
Lee Irvine <lee@kezzi.co>
2019-05-31 01:25:15 -0700

    Initial Commit

---
+++ clear-webview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clear-webview
+//
+//  Created by Lee Irvine on 5/31/19.
+//  Copyright © 2019 kezzi.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
